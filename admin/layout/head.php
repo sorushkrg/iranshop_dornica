01196d013448159abc2e4325bf24e538004aa84e
sorushkrg <sorush.krg@gmail.com>
2024-12-29 22:31:46 +0330

    nav_path is add

--- admin/layout/head.php
+++ admin/layout/head.php
@@ -7,17 +7,17 @@ $firstUrl = $_SERVER["HTTP_HOST"];
     <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
     <meta content="Themesdesign" name="author" />
     <!-- App favicon -->
-    <link rel="shortcut icon" href="assets/images/favicon.ico">
+    <link rel="shortcut icon" href="<?=$nav_path?>assets/images/favicon.ico">
     
     <!-- Bootstrap Css -->
-    <link href="assets/css/bootstrap-rtl.min.css" id="bootstrap-style" rel="stylesheet" type="text/css" />
+    <link href="<?=$nav_path?>assets/css/bootstrap-rtl.min.css" id="bootstrap-style" rel="stylesheet" type="text/css" />
     <!-- Icons Css -->
-    <link href="assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+    <link href="<?=$nav_path?>assets/css/icons.min.css" rel="stylesheet" type="text/css" />
     <!-- App Css-->
-    <link href="assets/css/app-rtl.min.css" id="app-style" rel="stylesheet" type="text/css" />
-    <link href="assets/fonts/fontawesome/css/all.min.html" rel="stylesheet" type="text/css"/>
+    <link href="<?=$nav_path?>assets/css/app-rtl.min.css" id="app-style" rel="stylesheet" type="text/css" />
+    <link href="<?=$nav_path?>assets/fonts/fontawesome/css/all.min.html" rel="stylesheet" type="text/css"/>
     <!-- Font Vazir-->
-    <link href="assets/css/font-vzir.css" rel="stylesheet" type="text/css"/>
-    <link href="assets/fontawesome/css/all.min.css" rel="stylesheet" type="text/css" />
+    <link href="<?=$nav_path?>assets/css/font-vzir.css" rel="stylesheet" type="text/css"/>
+    <link href="<?=$nav_path?>assets/fontawesome/css/all.min.css" rel="stylesheet" type="text/css" />
 
     
