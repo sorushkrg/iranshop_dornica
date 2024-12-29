f87b3c3f879165888ad3a5f60654728e5152cc72
sorushkrg <sorush.krg@gmail.com>
2024-12-29 22:30:56 +0330

    nave_path is add

--- admin/includeFiles.php
+++ admin/includeFiles.php
@@ -2,3 +2,7 @@
 @session_start();
 error_reporting(0);
 date_default_timezone_set("Asia/Tehran");
+if (strpos($_SERVER['PHP_SELF'], 'plugins') > 0)
+    $nav_path = '../../';
+else
+    $nav_path = '';
