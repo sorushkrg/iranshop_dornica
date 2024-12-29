9de531ffc5d78d113ce06d9e4f7d72990dbe8f40
sorushkrg <sorush.krg@gmail.com>
2024-12-29 22:32:09 +0330

    nav_path is add

--- admin/layout/sideBar.php
+++ admin/layout/sideBar.php
@@ -9,7 +9,7 @@
             <li class="menu-title">منو اصلی </li>
 
             <li>
-                <a href="index.php" class="waves-effect">
+                <a href="<?= $nav_path ?>index.php" class="waves-effect">
                     <i class="mdi mdi-view-dashboard"></i>
                     <span class="badge rounded-pill bg-primary float-end">2</span>
                     <span>داشبورد</span>
@@ -29,10 +29,35 @@
                     <span> احراز هویت </span>
                 </a>
                 <ul class="sub-menu" aria-expanded="false">
-                    <li><a href="pages-login.php">وارد شدن</a></li>
-                    <li><a href="pages-register.php">ثبت نام</a></li>
-                    <li><a href="pages-recoverpw.php">بازیابی رمز عبور</a></li>
-                    <li><a href="pages-lock-screen.php">صفحه قفل</a></li>
+                    <li><a href="<?= $nav_path ?>pages-login.php">وارد شدن</a></li>
+                    <li><a href="<?= $nav_path ?>pages-register.php">ثبت نام</a></li>
+                    <li><a href="<?= $nav_path ?>pages-recoverpw.php">بازیابی رمز عبور</a></li>
+                    <li><a href="<?= $nav_path ?>pages-lock-screen.php">صفحه قفل</a></li>
+                </ul>
+            </li>
+
+            <li>
+                <a href="javascript: void(0);" class="has-arrow waves-effect">
+                    <i class="ion ion-md-copy"></i>
+                    <span> بلاگ</span>
+                </a>
+                <ul class="sub-menu" aria-expanded="false">
+                    <li><a href="<?= $nav_path ?>plugins/blog/blog_add.php">درج</a></li>
+                    <li><a href="<?= $nav_path ?>plugins/blog/blog_edit.php">ویرایش</a></li>
+                    <li><a href="<?= $nav_path ?>plugins/blog/blog_delete.php">حذف</a></li>
+                    <li><a href="<?= $nav_path ?>plugins/blog/blog_list.php">لیست</a></li>
+                </ul>
+
+            <li>
+                <a href="javascript: void(0);" class="has-arrow waves-effect">
+                    <i class="ion ion-md-copy"></i>
+                    <span> بلاگ-دسته بندی </span>
+                </a>
+                <ul class="sub-menu" aria-expanded="false">
+                    <li><a href="<?= $nav_path ?>plugins/blogCategori/blogCategori_add.php">درج</a></li>
+                    <li><a href="<?= $nav_path ?>plugins/blogCategori/blogCategori_edit.php">ویرایش</a></li>
+                    <li><a href="<?= $nav_path ?>plugins/blogCategori/blogCategori_delete.php">حذف</a></li>
+                    <li><a href="<?= $nav_path ?>plugins/blogCategori/blogCategori_list.php">لیست</a></li>
                 </ul>
             </li>
 
