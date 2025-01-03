ed5688afa4871a5443e0c7197c72008eefa13124
sorushkrg <sorush.krg@gmail.com>
2025-01-03 23:29:10 +0330

    is add

---
+++ admin/layout/paging.php
@@ -0,0 +1,68 @@
+<?php function pg($title_pageNum_rs1, $pageNum_rs1, $currentPage, $queryString_rs1, $totalPages_rs1)
+{ ?>
+      <ul class="pagination">
+
+            <?php if ($pageNum_rs1 > 0) { // Show if not first page 
+            ?>
+                  <li> <a href="<?php printf("%s?%s=%d%s", $currentPage, $title_pageNum_rs1, 0, $queryString_rs1); ?>" data-popup="tooltip" title="اول">&lsaquo;&lsaquo;</a>
+                  <?php } // Show if not first page 
+                  ?>
+                  </li>
+                  <?php if ($pageNum_rs1 > 0) { // Show if not first page 
+                  ?>
+                        <li>
+                              <a href="<?php printf("%s?%s=%d%s", $currentPage, $title_pageNum_rs1, max(0, $pageNum_rs1 - 1), $queryString_rs1); ?>" data-popup="tooltip" title="قبلی">&lsaquo;</a>
+
+                        </li>
+                        <?php
+
+                        $safheghabli = $pageNum_rs1 - 1;
+                        $i = $safheghabli - 1;
+                        if ($i < 1)
+                              $i = 0;
+                        while ($i >= 0 and $i <= $safheghabli) {
+                        ?>
+                              <li>
+                                    <a href="<?php printf("%s?%s=%d%s", $currentPage, $title_pageNum_rs1, $i, $queryString_rs1); ?>"><?php echo $i + 1; ?></a>
+
+                              </li>
+                        <?php
+                              $i++;
+                        } ?>
+                  <?php } // Show if not first page 
+                  ?>
+                  <li class="active"><a href="#">
+                              <?php echo $pageNum_rs1 + 1; ?>
+                        </a>
+                  </li>
+                  <?php if ($pageNum_rs1 < $totalPages_rs1) { // Show if not last page 
+                  ?>
+                        <?php
+                        $i = 1;
+                        $safhebadi = $pageNum_rs1 + 1;
+                        while ($i < 3 and $safhebadi <= $totalPages_rs1) {
+                        ?>
+                              <li>
+                                    <a href="<?php printf("%s?%s=%d%s", $currentPage, $title_pageNum_rs1, $safhebadi, $queryString_rs1); ?>"><?php echo $safhebadi + 1; ?></a>
+
+                              </li>
+                        <?php
+                              $i++;
+                              $safhebadi++;
+                        } ?>
+                        <li>
+                              <a href="<?php printf("%s?%s=%d%s", $currentPage, $title_pageNum_rs1, min($totalPages_rs1, $pageNum_rs1 + 1), $queryString_rs1); ?>" data-popup="tooltip" title="بعدی">&rsaquo;</a>
+
+                        </li>
+                  <?php } // Show if not last page 
+                  ?>
+                  <li><?php if ($pageNum_rs1 < $totalPages_rs1) { // Show if not last page 
+                        ?>
+                              <a href="<?php printf("%s?%s=%d%s", $currentPage, $title_pageNum_rs1, $totalPages_rs1, $queryString_rs1); ?>" data-popup="tooltip" title="آخر">&rsaquo;&rsaquo;</a>
+                        <?php } // Show if not last page 
+                        ?>
+                  </li>
+
+
+      </ul>
+<?php } ?>
