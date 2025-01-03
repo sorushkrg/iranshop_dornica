33e11aec247aedc10407770767c8a00dc17ed857
sorushkrg <sorush.krg@gmail.com>
2025-01-04 00:13:59 +0330

    authors_delete is add

---
+++ admin/plugins/authors/authors_delete.php
@@ -0,0 +1,107 @@
+<?php require_once "../../checkLogin.php" ?>
+
+<!doctype html>
+<html lang="en" dir="rtl">
+
+
+<!-- Mirrored from theme-script.ir/templates/lexa/Lexa-RTL/RTL-lexa-teal/pages-blank.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 27 Dec 2024 17:20:04 GMT -->
+<head>
+
+<?php require_once "../../layout/head.php" ?>
+    
+    <title>صفحه خالی | لکسا - قالب مدیریت و داشبورد</title>
+</head>
+
+
+<body data-sidebar="dark">
+
+<!-- <body data-layout="horizontal" data-topbar="colored"> -->
+
+<!-- Begin page -->
+<div id="layout-wrapper">
+
+    <?php require_once "../../layout/header.php"?>
+    <!-- ========== Left Sidebar Start ========== -->
+    <div class="vertical-menu">
+
+        <div data-simplebar class="h-100">
+
+            <!--- Sidemenu -->
+            <?php require_once "../../layout/sideBar.php" ?>
+            <!-- Sidebar -->
+        </div>
+    </div>
+    <!-- Left Sidebar End -->
+
+
+    <!-- ============================================================== -->
+    <!-- Start right Content here -->
+    <!-- ============================================================== -->
+    <div class="main-content">
+
+        <div class="page-content">
+            <div class="container-fluid">
+
+                <!-- start page title -->
+                <div class="row">
+                    <div class="col-sm-6">
+                        <div class="page-title-box">
+                            <h4>صفحه خالی</h4>
+                            <ol class="breadcrumb m-0">
+                                <li class="breadcrumb-item"><a href="javascript: void(0);">لکسا</a></li>
+                                <li class="breadcrumb-item"><a href="javascript: void(0);">صفحات</a></li>
+                                <li class="breadcrumb-item active">صفحه خالی</li>
+                            </ol>
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="state-information d-none d-sm-block">
+                            <div class="state-graph">
+                                <div id="header-chart-1"></div>
+                                <div class="info">تعادل  2,317 تومان </div>
+                            </div>
+                            <div class="state-graph">
+                                <div id="header-chart-2"></div>
+                                <div class="info">مورد فروخته شده 1230</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- end page title -->
+
+            </div> <!-- container-fluid -->
+        </div>
+        <!-- End Page-content -->
+
+
+        <footer class="footer">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-sm-12">
+                        © <script>document.write(new Date().getFullYear())</script> لکسا <span class="d-none d-sm-inline-block">   - ساخته شده با <i class="mdi mdi-heart text-danger"></i> توسط تم دیزاین</span>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </div>
+    <!-- end main content-->
+
+</div>
+<!-- END layout-wrapper -->
+<!-- Right bar overlay-->
+<div class="rightbar-overlay"></div>
+
+<!-- JAVASCRIPT -->
+<script src="../../assets/libs/jquery/jquery.min.js"></script>
+<script src="../../assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="../../assets/libs/metismenu/metisMenu.min.js"></script>
+<script src="../../assets/libs/simplebar/simplebar.min.js"></script>
+<script src="../../assets/libs/node-waves/waves.min.js"></script>
+<script src="../../assets/libs/jquery-sparkline/jquery.sparkline.min.js"></script>
+
+<script src="../../assets/js/app.js"></script>
+
+</body>
+
+<!-- Mirrored from theme-script.ir/templates/lexa/Lexa-RTL/RTL-lexa-teal/pages-blank.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 27 Dec 2024 17:20:04 GMT -->
+</html>
